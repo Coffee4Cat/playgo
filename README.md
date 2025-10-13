e58bf2d7cdcb79dcb9c58942bd84371da81e487c
MiloszPiecha <milosz.piecha05@gmail.com>
2025-10-13 11:47:19 +0200

    test_commit

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # PLAYGO
-![Language](https://img.shields.io/badge/Language-Go-cyan)  
-![Status](https://img.shields.io/badge/Status-InProgress-orange)  
-Go project for TUI audio player
+### Short description
+A terminal-based music player that allows you to browse and play your music collection directly from the command line.
+Designed for use in SSH sessions.
+
+
+
+### Future Development
+- [ ] AutoPlay
 
 
 ## Third-party libraries
-This project uses the following third-party libraries:
+This project is licensed under the MIT License.
+
+Third-Party Licenses
+This software includes open-source components:
 
 - [go-mp3](https://github.com/hajimehoshi/go-mp3) – Licensed under Apache 2.0
 - [oto](https://github.com/hajimehoshi/oto) – Licensed under Apache 2.0
+
+All third-party libraries retain their original licenses and copyright notices.
--- playgo.go
+++ playgo.go
@@ -1,43 +1,41 @@
 package main
 
 import (
-	"log"
 	"flag"
-	"playgo/user_interface"
+	"log"
+	"os"
+	"path/filepath"
 	"playgo/audio_player"
 	"playgo/structures"
+	"playgo/user_interface"
 	"time"
 )
 
-
-
 func main() {
-	audiopath := flag.String("f", "/home/milosz/Music", "Absolute path to the folder with music")
+	home, _ := os.UserHomeDir()
+	audiopath := flag.String("f", filepath.Join(home, "Music"), "Absolute path to the folder with music")
 	flag.Parse()
-	
+
 	log.Println("[PLAYGO][VERSION 1.0]")
 	time.Sleep(time.Millisecond * 100)
 	log.Println("[PLAYGO][PATH:" + *audiopath + "]")
 	time.Sleep(time.Millisecond * 100)
 	log.Println("[PLAYGO][HAVE FUN!]")
 	time.Sleep(time.Millisecond * 500)
-	
+
 	// DIRECTORY SEARCH
 	var folders []structures.AudioFolder = audio_player.ListAudioFolders(*audiopath)
 	for i, _ := range folders {
-		var files []structures.AudioFile = audio_player.ListAudioFiles(*audiopath,&folders[i])
+		var files []structures.AudioFile = audio_player.ListAudioFiles(*audiopath, &folders[i])
 		folders[i].UpdateAudioFiles(files)
 	}
-	
-
 
 	var commandChannel chan structures.PlayerCommand
 	var feedbackChannel chan structures.PlayerCommand
-	
+
 	commandChannel = make(chan structures.PlayerCommand)
 	feedbackChannel = make(chan structures.PlayerCommand)
-	
-	
+
 	audio_player.InitializePlayer(commandChannel, feedbackChannel)
 	user_interface.InitializeUI(&folders, commandChannel, feedbackChannel)
 
