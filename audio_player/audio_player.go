f95ce70109c5ba68c035d5d09e1b2d61691f97a1
MiloszPiecha <milosz.piecha05@gmail.com>
2025-11-10 20:10:35 +0100

    playbutton_fixed

--- audio_player/audio_player.go
+++ audio_player/audio_player.go
@@ -1,16 +1,16 @@
 package audio_player
 
 import (
-	"github.com/hajimehoshi/oto"
 	"fmt"
 	"io"
 	"playgo/structures"
-	"time"
+	"sync"
+
+	"github.com/hajimehoshi/oto"
 )
 
-
-var totalBytesRead int64 = 0
-
+var playbutton_mutex sync.Mutex
+var playspeed int = 44100
 
 func InitializePlayer(command chan structures.PlayerCommand, feedback chan structures.PlayerCommand) {
 	var audiofile *structures.AudioFile
@@ -19,17 +19,19 @@ func InitializePlayer(command chan structures.PlayerCommand, feedback chan struc
 	var playing bool
 	var volume float64 = 0.5
 
-
 	go func() {
 		buf := make([]byte, 4096)
-		ctx, _ = oto.NewContext(44100, 2, 2, 5000) // Well... Cannot think bout better solution right now
+		ctx, _ = oto.NewContext(playspeed, 2, 2, 4096) // Well... Cannot think bout better solution right now
 		for {
 			select {
 			case cmd := <-command:
 				switch cmd.Action {
 				case structures.ActionSetPlay:
+					playbutton_mutex.Lock()
 					playing = !playing
+					playbutton_mutex.Unlock()
 				case structures.ActionSetTrack:
+					playbutton_mutex.Lock()
 					if player != nil {
 						player.Close()
 					}
@@ -40,6 +42,7 @@ func InitializePlayer(command chan structures.PlayerCommand, feedback chan struc
 					audiofile.ResetDecoder()
 					player = ctx.NewPlayer()
 					playing = true
+					playbutton_mutex.Unlock()
 				case structures.ActionSetLevel:
 					if cmd.Level {
 						volume += 0.1
@@ -58,7 +61,6 @@ func InitializePlayer(command chan structures.PlayerCommand, feedback chan struc
 				if playing && ctx != nil && audiofile != nil {
 					n, err := audiofile.Decoder.Read(buf)
 					if n > 0 {
-				        totalBytesRead += int64(n)
 						for i := 0; i < n; i += 2 {
 							sample := int16(buf[i]) | int16(buf[i+1])<<8
 							adjusted := int16(float64(sample) * volume)
@@ -66,28 +68,28 @@ func InitializePlayer(command chan structures.PlayerCommand, feedback chan struc
 							buf[i+1] = byte((adjusted >> 8) & 0xFF)
 						}
 						player.Write(buf[:n])
-						audiofile.CurrentTime += int(float64(n) * 1000 / (4.0 * 44100.0))
+						audiofile.CurrentTime += int(float64(n) * 1000 / (4.0 * float64(playspeed)))
 						if audiofile.CurrentTime > audiofile.Duration {
 							audiofile.CurrentTime = audiofile.Duration
 						}
 					}
 					if err == io.EOF {
+						playbutton_mutex.Lock()
 						playing = false
+						playbutton_mutex.Unlock()
 						audiofile.CurrentTime = 0
 						feedback <- structures.PlayerCommand{Action: structures.ActionSetPlayFeedback, Play: playing}
 						continue
 					}
 					if err != nil {
 						fmt.Println("Decoder error:", err)
+						playbutton_mutex.Lock()
 						playing = false
+						playbutton_mutex.Unlock()
 						feedback <- structures.PlayerCommand{Action: structures.ActionSetPlayFeedback, Play: playing}
 					}
-				} else {
-					time.Sleep(10 * time.Millisecond)
-					playing = false
-					// feedback <- structures.PlayerCommand{Action: structures.ActionSetPlayFeedback, Play: playing}
-					totalBytesRead = 0
 				}
+
 			}
 		}
 	}()
